0335a594dfc20fc30a2f5ad4382c65a8c3c71694
LeopoldArkham <leopold.arkham@gmail.com>
2017-03-06 14:53:19 +0100

    Added ws checkers

--- src/tomlchar.rs
+++ src/tomlchar.rs
@@ -4,7 +4,8 @@ pub trait TOMLChar {
     fn is_bare_key_char(&self) -> bool;
     fn is_ws_or_equal(&self) -> bool;
     fn is_int_float_char(&self) -> bool;
-    fn not_whitespace(&self) -> bool;
+    fn is_ws(&self) -> bool;
+    fn not_ws(&self) -> bool;
     fn not_whitespace_or_pound(&self) -> bool;
 }
 
@@ -30,11 +31,15 @@ impl TOMLChar for char {
         }
     }
 
-    fn not_whitespace(&self) -> bool {
+    fn is_ws(&self) -> bool {
         match *self {
-            ' ' | '\t' => false,
-            _ => true,
+            ' ' | '\t' => true,
+            _ => false,
         }
+    }
+
+    fn not_ws(&self) -> bool {
+        !self.is_ws()
     }
 
     fn not_whitespace_or_pound(&self) -> bool {
