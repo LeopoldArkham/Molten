d24fb21f00fad53b2ebc60eb9ac8818f11a5eb19
Mark H. Colburn <colburn.mark@gmail.com>
2017-12-06 13:03:26 -0800

    Make benchmark code conditional on nightly build.

--- benches/parser_benchmark.rs
+++ benches/parser_benchmark.rs
@@ -1,5 +1,7 @@
 //! Parser benchmark tests to identify any performance regressions
 //! in the parser.
+#![feature(test)]
+#![cfg(feature = "nightly")]
 
 // For details on using Criterion, see the documentation at
 // https://japaric.github.io/criterion.rs/book/index.html
@@ -7,10 +9,11 @@ extern crate criterion;
 extern crate Molten;
 
 use criterion::{Criterion, Bencher};
-use std::fs::File;
-use std::io::Read;
 
 fn parse_full(b: &mut Bencher) -> () {
+    use std::fs::File;
+    use std::io::Read;
+
     let path = "tests/reproduction/full.toml";
     let mut content = String::new();
     let mut f = File::open(&path).unwrap();
